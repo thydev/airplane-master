2543351aed8eb785e4e995ce1a7128cb22b2962f
Chanthy Ngin <nginchanthy@gmail.com>
2017-12-08 22:53:38 -0800

    Remove both an enemy and a missile after hitting

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+
 var setting = {
-    moveSpeed: 10,
+    moveSpeed: 20,
     oceanWidth: 900,
-    maxPlayerTop: 450,
+    maxPlayerTop: 650,
     maxPlayerBottom: 700,
     missileSpeed: 20,
-    enemySpeed: 10
+    enemySpeed: 1
 }
 
 let game = {
@@ -25,6 +26,11 @@ const enemy = {
     height: 75
 }
 
+const missile = {
+    width: 4,
+    height: 10
+}
+
 var enemies = [
     { left: 100, top: 20 },
     { left: 180, top: 20 },
@@ -33,12 +39,12 @@ var enemies = [
 ];
 
 var missiles = [];
+var timeOutId; //Use to stop the game loop
 
 function drawPlayer() {
     var content = "<div class='player' style='left: " + player.left + "px; top: " + player.top + "px;'></div>";
     document.getElementById('players').innerHTML = content;
 }
-//drawPlayer();
 
 function drawEnemies() {
     var content = "";
@@ -47,12 +53,19 @@ function drawEnemies() {
     }
     document.getElementById('enemies').innerHTML = content;
 }
-//drawEnemies();
 
 function moveEnemies() {
-    for (var i = 0; i < enemies.length; i++) {
-        enemies[i].top += setting.enemySpeed;
+    if (enemies.length > 0) {
+        for (var i = 0; i < enemies.length; i++) {
+            enemies[i].top += setting.enemySpeed;
+            if (enemies[i].top + enemy.height >= setting.maxPlayerTop) {
+                gameStop();
+            }
+        }
+    } else {
+        generateEnemies();
     }
+    
 }
 
 function drawMissiles() {
@@ -64,57 +77,88 @@ function drawMissiles() {
 }
 
 function moveMissiles() {
+    let activeMissile = [];
     for (var i = 0; i < missiles.length; i++) {
         missiles[i].top -= setting.missileSpeed;
+        if (!(missiles[i].top <= 0)) {
+            //Keep only the missiles that are still below the top screen
+            
+            let activeEnemies = [];
+            let isHit = false;
+            for (let j = 0; j < enemies.length; j++){
+                if (!isMissileHit(enemies[j].top, enemies[j].left,enemy.width, enemy.height,
+                    missiles[i].top, missiles[i].left, missile.width)) {
+                     activeEnemies.push(enemies[j]);   
+
+                    } else {
+                        //Yeah an enemy has been destroyed
+                        game.score++;
+                        isHit = true;
+                        //break;
+                    }
+            }
+            if (!isHit) {
+                activeMissile.push(missiles[i]);
+            }
+            enemies = activeEnemies;
+            drawEnemies();
+            updateGameState();
+
+        } else {
+            
+            
+        }
     }
+    missiles = activeMissile;
 }
 
 document.onkeydown = function (e) {
-    if (e.keyCode === 37 && player.left >= setting.moveSpeed) { //left
+    if (game.start) {
+        if (e.keyCode === 37 && player.left >= setting.moveSpeed) { //left
 
-        player.left -= setting.moveSpeed;
+            player.left -= setting.moveSpeed;
 
-    } else if (e.keyCode === 39 && player.left <= setting.oceanWidth - player.width) { //right
-        player.left += setting.moveSpeed;
-    } else if (e.keyCode === 40 && player.top <= setting.maxPlayerBottom - player.height) { //down
-        player.top += setting.moveSpeed;
-    } else if (e.keyCode === 38 && player.top >= setting.maxPlayerTop) { //up
-        player.top -= setting.moveSpeed;
-    } else if (e.keyCode === 32) { // Spacebar = fire
-        missiles.push({ left: (player.left + player.width / 2), top: (player.top - 5) });
-        console.log(missiles);
-        moveMissiles();
-        drawMissiles();
+        } else if (e.keyCode === 39 && player.left <= setting.oceanWidth - player.width) { //right
+            player.left += setting.moveSpeed;
+        } else if (e.keyCode === 40 && player.top <= setting.maxPlayerBottom - player.height) { //down
+            player.top += setting.moveSpeed;
+        } else if (e.keyCode === 38 && player.top >= setting.maxPlayerTop) { //up
+            player.top -= setting.moveSpeed;
+        } else if (e.keyCode === 32) { // Spacebar = fire
+            missiles.push({ left: (player.left + player.width / 2), top: (player.top - 5) });
+            console.log(missiles);
+            moveMissiles();
+            drawMissiles();
 
+        }
+        drawPlayer();
     }
-    drawPlayer();
 }
 
-var gameLoop; //Use to stop the game loop
+
 
 function gameStart() {
-    //console.log('gameLoop');
-    if (!game.start) {
-        gameStop();
+    //console.log('timeOutId');
+    //console.log("Start", game.start);
+    if (game.start) {
+
+        drawPlayer();
+        moveEnemies();
+        drawEnemies();
+        moveMissiles();
+        drawMissiles();
+        //generateEnemies();
+        timeOutId = setTimeout(() => {
+            gameStart()
+        }, 10);
     }
-
-    drawPlayer();
-
-    moveEnemies();
-    drawEnemies();
-
-    moveMissiles();
-    drawMissiles();
-    collisionDetector();
-
-    gameLoop = setTimeout(() => {
-        gameStart()
-    }, 500);
 }
 
 function gameStop() {
-    clearTimeout(gameLoop);
     game.start = false;
+    clearTimeout(timeOutId);
+    clearTimeout(genEnemiesTime)
+    console.log("stop!");
 }
 
 window.onload = function () {
@@ -122,17 +166,34 @@ window.onload = function () {
     gameStart();
 };
 
-function collisionDetector() {
-    console.log('collide?');
-    let plane = enemies[0];
-    if (missiles.length > 0) {
-        
-        let missile = missiles[0];
-        console.log(`plane:${plane.left}, ${plane.top}`);
-        console.log(`missile:${missile.left}, ${missile.top}`);
-        if (plane.top + enemy.height >= missile.top &&
-            plane.left <= missile.left && missile.left <= plane.left + enemy.width) {
-            console.log('Destroy');
+function isMissileHit(planeTop, planeLeft, planeWidth, planeHeight,
+                        missileTop, missileLeft, missileWidth) {
+    
+        if (planeTop + planeHeight >= missileTop &&
+            planeLeft <= missileLeft + missileWidth && 
+            missileLeft <= planeLeft + planeWidth) {
+            //console.log('Destroy');
+            return true;
+        }
+        return false;
+    
+}
+
+function updateGameState() {
+    document.getElementById('score').innerHTML = game.score;
+    
+}
+
+var genEnemiesTime;
+function generateEnemies() {
+    const newEnemies = Math.floor(Math.random() * 10) + 1; //Random number from 1 to 10
+    let left = 0;
+    for(let i = 0; i < newEnemies; i++){
+        left += enemy.width + Math.floor(Math.random() * 10) + 10;
+        if (left  <= setting.oceanWidth) {
+            enemies.push({ left: left, top:20 });
         }
     }
+    console.log(enemies);
+    
 }
